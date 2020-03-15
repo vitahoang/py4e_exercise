a6318d358611ce027476648c3a7ce59ad46ab343
Vita Hoang <hunghoang.vita@gmail.com>
2020-03-15 23:06:38 +0700

    Completed Lesson 12

---
+++ exc-12-08.py
@@ -0,0 +1,23 @@
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+l_tag = soup("span")
+x_count = 0
+for tag in l_tag:
+    x_count = x_count + (int(tag.contents[0]))
+print(x_count)
---
+++ exc-12-09.py
@@ -0,0 +1,22 @@
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+import re
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = 'http://py4e-data.dr-chuck.net/known_by_Martine.html'
+for i in range(7):
+	html = urllib.request.urlopen(url, context=ctx).read()
+	soup = BeautifulSoup(html, 'html.parser')
+	l_tag = soup("a")
+	url = re.findall('href="(.*)">', str(l_tag[17]))[0]
+	x_name = l_tag[17].contents
+	print(x_name[0])
