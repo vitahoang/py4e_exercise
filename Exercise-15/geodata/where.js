f0b01921e81014bdfeadb9426fb142a37967e7e7
Vita Hoang <hunghoang.vita@gmail.com>
2020-03-26 22:25:50 +0700

    mission completed

---
+++ Exercise-15/geodata/where.js
@@ -0,0 +1,20 @@
+myData = [
+[21.0293646,105.8361633, '58 Quốc Tử Giám, Văn Miếu, Đống Đa, Hà Nội 100000, Vietnam'],
+[21.0238349,105.8227395, '418 Đường La Thành, Chợ Dừa, Đống Đa, Hà Nội, Vietnam'],
+[21.040633,105.7985155, 'Nguyễn Văn Huyên, Quan Hoa, Cầu Giấy, Hà Nội 100000, Vietnam'],
+[20.9945438,105.8677181, 'Vinhomes Times City, Vinh Tuy, Hai Bà Trưng District, Hanoi, Vietnam'],
+[10.7427497,106.6119309, '1 Đường Số 17A, Bình Trị Đông B, Bình Tân, Hồ Chí Minh, Vietnam'],
+[21.0714407,105.7912847, 'Xuân Đỉnh, Từ Liêm, Hà Nội, Vietnam'],
+[42.5310175,-86.2278331, 'Wau-Ke-Na, William Erby Smith Preserve, 1599 Lakeshore Dr, Fennville, MI 49408, USA'],
+[16.0563276,108.2008338, 'Da Nang International Airport, p Duy Tân, Hòa Thuận Tây, Hải Châu, Đà Nẵng 550000, Vietnam'],
+[16.0676043,108.244753, 'Belle Maison Parosand Danang, 216 Võ Nguyên Giáp, Phước Mỹ, Sơn Trà, Đà Nẵng 550000, Vietnam'],
+[41.578745,-123.0919855, 'Marble Mountains, California 96032, USA'],
+[15.8748973,108.3398147, 'Cồn Hến, 200 Nguyễn Tri Phương, rẽ trái, Hội An, Quảng Nam, Vietnam'],
+[15.5897126,108.5530566, 'Thôn Hạ Thanh 1, Tam Thanh, Tam Kỳ, Quảng Nam, Vietnam'],
+[10.8184631,106.6588245, 'Tan Son Nhat International Airport, Trường Sơn, Phường 2, Tân Bình, Hồ Chí Minh, Vietnam'],
+[10.7855522,106.7098272, 'Vinhomes Golden River Sài Gòn, Bến Nghé, District 1, Ho Chi Minh City, Vietnam'],
+[10.7881464,106.6938191, 'Le Van Tam Park, Võ Thị Sáu, Đa Kao, Quận 1, Hồ Chí Minh 700000, Vietnam'],
+[37.226291,-95.74735079999999, '3222 W Main St, Independence, KS 67301, USA'],
+[10.7745379,106.6924421, 'Tao Đàn Park, Trương Định, Phường Bến Thành, Quận 1, Hồ Chí Minh, Vietnam'],
+[21.0356011,105.8326289, '19 Ngọc Hà, Đội Cấn, Ba Đình, Hà Nội 10000, Vietnam']
+];
